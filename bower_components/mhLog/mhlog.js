3315614d391672e02e609857474885c1370f5d28
Michael Haungs <mhaungs@calpoly.edu>
2014-04-12 17:05:54 -0700

    Adding updated files.

--- bower_components/mhLog/mhlog.js
+++ bower_components/mhLog/mhlog.js
@@ -1,4 +1,28 @@
 /*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2013 Michael Haungs <mhaungs at calpoly.edu>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/*
  * mhLog
  *
  * Description: A simple utility to control log output.  You can optionally display
@@ -7,7 +31,12 @@
  * @author mhaungs
  */
 
-	/*jshint globalstrict: true*/
+//FIXME:  should upgrade module structure.  (see mhGeo.js)
+
+/* global exports: true */
+module.exports = function() {
+
+    /*jshint globalstrict: true*/
     "use strict";  // EMCAScript 5 pragma to catch more javascript errors
 
     /*
@@ -49,7 +78,7 @@
             if (level >= currentLevel) {
                 if (strace === true)
                     logCallTrace();
-                console.log("mhlog: Message: " + msg);
+                console.log("mhlog: [" + timestamp() + "] Message: " + msg);
                 return msg;
             }
         }
@@ -87,13 +116,20 @@
         }
     }
 
+    function timestamp() {
+        var ts = new Date();
+        return ts;
+    }
+
     /*
      ************ Exports *************
      */
 
-	/* global exports: true */
-    exports.LEVEL = LEVEL;
-    exports.setShowStackTrace = setShowStackTrace;
-    exports.setLoggingLevel = setLoggingLevel;
-    exports.log = log;
+    return {
+        LEVEL: LEVEL,
+        setShowStackTrace: setShowStackTrace,
+        setLoggingLevel: setLoggingLevel,
+        log: log
+    };
 
+};
